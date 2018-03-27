7840f36924ad99d87edded0131030c2fba4803ea
vokulnin <32674624+vokulnin@users.noreply.github.com>
2018-03-27 12:36:59 +0400

    message

---
+++ app/src/main/java/com/example/vokulnin/cassebrique/Myview.java
@@ -0,0 +1,40 @@
+package com.example.vokulnin.cassebrique;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Myview extends View {
+    public Game main;
+    public Myview(Context context , Game Main) {
+        super(context);
+       main = Main;
+    }
+
+    public Myview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Myview(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // TODO Auto-generated method stub
+        super.onDraw(canvas);
+
+        RectF raquette = new RectF(main.raquette.pos_X, main.raquette.pos_Y, main.raquette.pos_X + main.raquette.size_X,main.raquette.pos_Y+  main.raquette.size_Y);
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.GRAY);
+        canvas.drawRect(raquette,paint);
+        canvas.drawRect(main.balle.Draw(),paint);
+
+    }
+
+}
